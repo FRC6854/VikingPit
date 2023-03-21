3f82c4a8d14c97947a299218683f761d9498bb20
Owen Shaule <owen.shaule@gmail.com>
2023-03-20 20:49:23 -0400

    Starting out Jinja and Flask

---
+++ vikingdash/__init__.py
@@ -0,0 +1,13 @@
+"""
+VikingDash - By FRC6854
+Under the CC0 License
+"""
+
+__name__ = "VikingDash"
+__authors__ = ["Owen Shaule"]
+__license__ = "CC0"
+
+from flask import Flask
+
+app = Flask(__name__)
+
---
+++ wsgi.py
@@ -0,0 +1,1 @@
+from vikingdash import app
